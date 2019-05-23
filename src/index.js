f311c297fae0a781c8300638bb3f4899d78a2319
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-05-23 01:34:12 -0700

    provide the redux store to the top level App component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from 'redux';
+import rootReducer from './reducer';
+import jwtDecode from 'jwt-decode';
+import { setCurrentUser,setAuthorizationToken } from './actions';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+if (localStorage.jwtToken) {
+  setAuthorizationToken(localStorage.jwtToken);
+  // prevent someone from manually setting a key of 'jwtToken' in localStorage
+  try {
+    store.dispatch(setCurrentUser(jwtDecode(localStorage.jwtToken)));
+  } catch(e) {
+    store.dispatch(setCurrentUser({}))
+  }
+}
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+  ,
+  document.getElementById('root')
+);
